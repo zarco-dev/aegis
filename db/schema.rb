893f6e65acf5545a8329d8a8c28d1d4f9df26e0c
Sebastian Restrepo <sebastian.respo@gmail.com>
2022-10-14 08:52:57 -0500

    Models v1.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_234505) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_135116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -125,8 +125,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_234505) do
     t.datetime "birth_date"
     t.string "cel_number"
     t.bigint "subscription_plan_id", null: false
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["subscription_plan_id"], name: "index_users_on_subscription_plan_id"

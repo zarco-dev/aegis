43f11f59cee84c5110f06bb1ccf99279550b44d7
Sebastian Restrepo <sebastian.respo@gmail.com>
2022-10-19 22:17:26 -0500

    navbar and signup views

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+# aegis
+
+**aegis** es un aplicativo web especializado en el almacenamiento de credenciales tanto web como personales, con un apartado facil de usar, con opcion de compartir contraseñas y crear grupos para planes ***Premium*** para un mejor control de las credenciales.
+
+Ademas poseemos un poderoso genereador de contraseñas seguras al estandar de los mercados web.
+
+![aegis logo](app/assets/images/isologotipo.png)
---
+++ app/javascript/controllers/signup_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="signup"
+export default class extends Controller {
+
+  static targets = ["basic", "premium", "buttonpremium", "buttonbasic", "subscontainer", "signup", "titleplan", "premiumuser"]
+  // static values = ["buttonpremium"]
+
+
+  hover() {
+    this.basicTarget.classList.add("scale-up");
+    this.premiumTarget.classList.add("scale-reduce");
+  }
+
+  hoverp(){
+    this.basicTarget.classList.remove("scale-up");
+    this.premiumTarget.classList.remove("scale-reduce");
+  }
+
+  submitbasic(){
+    this.subscontainerTarget.classList.add("subscription-container-display");
+    this.signupTarget.classList.remove("form-sign-up");
+    this.titleplanTarget.innerText = "Basic";
+  }
+
+  submitpremium() {
+    this.subscontainerTarget.classList.add("subscription-container-display");
+    this.signupTarget.classList.remove("form-sign-up");
+    this.titleplanTarget.innerText = "Premium";
+    this.premiumuserTarget.innerHTML = `<input autocomplete="off" type="hidden" value="true" name="user[premium_user]" id="user_premium_user">`;
+  }
+
+}

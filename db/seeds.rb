3e9d492eb9cf2597e0b9cc10438c979693ea3246
Sebastian Restrepo <sebastian.respo@gmail.com>
2022-10-21 12:59:57 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.destroy_all
+# User.destroy_all
 
 SubscriptionPlan.create!(
   title: "basic",
@@ -12,83 +12,83 @@ SubscriptionPlan.create!(
   price: 30
 )
 
-# # 6.times do |c|
-# #   user1 = User.create!(
-# #     username: Faker::Internet.username,
-# #     email: Faker::Internet.email,
-# #     password: "123456",
-# #     full_name: Faker::Name.name,
-# #     premium_user: false,
-# #     group_user: false,
-# #     birth_date: Faker::Date.birthday(min_age: 18, max_age: 65),
-# #     cel_number: Faker::Number.number
-# #   )
+# # # 6.times do |c|
+# # #   user1 = User.create!(
+# # #     username: Faker::Internet.username,
+# # #     email: Faker::Internet.email,
+# # #     password: "123456",
+# # #     full_name: Faker::Name.name,
+# # #     premium_user: false,
+# # #     group_user: false,
+# # #     birth_date: Faker::Date.birthday(min_age: 18, max_age: 65),
+# # #     cel_number: Faker::Number.number
+# # #   )
 
-# #   subs = Subscription.create!(
-# #     begining_date: "2022-10-14",
-# #     expired_date: "2022-11-14",
-# #     subscription_plan_id: rand(1..2),
-# #     user_id: c + 1
-# #   )
+# # #   subs = Subscription.create!(
+# # #     begining_date: "2022-10-14",
+# # #     expired_date: "2022-11-14",
+# # #     subscription_plan_id: rand(1..2),
+# # #     user_id: c + 1
+# # #   )
 
-# #   if subs.subscription_plan_id == 2
-# #     user1.premium_user = true
-# #     user1.save
-# #   end
-# # end
+# # #   if subs.subscription_plan_id == 2
+# # #     user1.premium_user = true
+# # #     user1.save
+# # #   end
+# # # end
 
 
-# # User.all.each do |user|
-# #   if user.premium_user
-# #     rand(1..3).times do
-# #       Group.create!(
-# #         name: %w[Streaming family socialmedia lospanas workteam].sample,
-# #         description: Faker::Lorem.paragraph,
-# #         users_number: 0,
-# #         user_id: user.id
-# #       )
-# #     end
-# #   end
-# # end
+# # # User.all.each do |user|
+# # #   if user.premium_user
+# # #     rand(1..3).times do
+# # #       Group.create!(
+# # #         name: %w[Streaming family socialmedia lospanas workteam].sample,
+# # #         description: Faker::Lorem.paragraph,
+# # #         users_number: 0,
+# # #         user_id: user.id
+# # #       )
+# # #     end
+# # #   end
+# # # end
 
-# # 4.times do
-# #   Password.create!(
-# #     url: Faker::Internet.url,
-# #     site_name: Faker::Internet.domain_word,
-# #     category: %w[socialmedia streaming bets courses].sample,
-# #     site_username: Faker::Internet.username,
-# #     site_password: "123456",
-# #     isgroup: false,
-# #     user_id: rand(1..6)
-# #   )
-# # end
+# # # 4.times do
+# # #   Password.create!(
+# # #     url: Faker::Internet.url,
+# # #     site_name: Faker::Internet.domain_word,
+# # #     category: %w[socialmedia streaming bets courses].sample,
+# # #     site_username: Faker::Internet.username,
+# # #     site_password: "123456",
+# # #     isgroup: false,
+# # #     user_id: rand(1..6)
+# # #   )
+# # # end
 
-# # # GroupInvitation.create!(
-# # #   status:
-# # # )
+# # # # GroupInvitation.create!(
+# # # #   status:
+# # # # )
 
-# # 4.times do |c|
-# #   Password.create!(
-# #     url: Faker::Internet.url,
-# #     site_name: Faker::Internet.domain_word,
-# #     category: %w[socialmedia streaming bets courses].sample,
-# #     site_username: Faker::Internet.username,
-# #     site_password: "123456",
-# #     isgroup: true,
-# #     user_id: rand(1..6),
-# #     group_id: Group.all.sample.id
-# #   )
-# # end
+# # # 4.times do |c|
+# # #   Password.create!(
+# # #     url: Faker::Internet.url,
+# # #     site_name: Faker::Internet.domain_word,
+# # #     category: %w[socialmedia streaming bets courses].sample,
+# # #     site_username: Faker::Internet.username,
+# # #     site_password: "123456",
+# # #     isgroup: true,
+# # #     user_id: rand(1..6),
+# # #     group_id: Group.all.sample.id
+# # #   )
+# # # end
 
-# # 2.times do
-# #   BankPassword.create!(
-# #     cardholder: Faker::Name.name,
-# #     account_type: %w[credit debit].sample,
-# #     bank_name: %w[Bancolombia Davivienda BBVA Falabella NuBank Popular Uala LuloBank Itau].sample,
-# #     franchise: %w[Visa Mastercard].sample,
-# #     account_number: Faker::Bank.account_number(digits: 16),
-# #     secure_code: rand(200..900),
-# #     expiration_date: "#{rand(2023..2029)}-#{rand(1..12)}-#{rand(1..30)}",
-# #     user_id: rand(1..6)
-# #   )
-# # end
+# # # 2.times do
+# # #   BankPassword.create!(
+# # #     cardholder: Faker::Name.name,
+# # #     account_type: %w[credit debit].sample,
+# # #     bank_name: %w[Bancolombia Davivienda BBVA Falabella NuBank Popular Uala LuloBank Itau].sample,
+# # #     franchise: %w[Visa Mastercard].sample,
+# # #     account_number: Faker::Bank.account_number(digits: 16),
+# # #     secure_code: rand(200..900),
+# # #     expiration_date: "#{rand(2023..2029)}-#{rand(1..12)}-#{rand(1..30)}",
+# # #     user_id: rand(1..6)
+# # #   )
+# # # end

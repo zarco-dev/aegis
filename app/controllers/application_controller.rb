b3a7cdff6affc8cfc48436789f61f9c04ab2dccc
Sebastian Restrepo <sebastian.respo@gmail.com>
2022-10-24 15:00:50 -0500

    dashboard path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,10 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
   private
 
   def skip_pundit?
